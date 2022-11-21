4e289039693e320dc8b56a8c76cf96f5382266d2
Liniya Merin Mathew <Liniya@NTP-LAP-788.chn.nestgroup.net>
2022-11-21 12:58:40 +0530

    Reverse

--- src/Main.java
+++ src/Main.java
@@ -86,6 +86,19 @@ public class Main {
         else
             System.out.println(num + " is odd");
     }
+    public static void Reverse() {
+        System.out.print("Enter an integer number:");
+        int number = new Scanner(System.in).nextInt();
+        int reverse = 0;
+        int tempNumber = number;
+        while(number > 0) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+        System.out.print("The reverse of the given number is = "+reverse);
+
+    }
 
 
 }
