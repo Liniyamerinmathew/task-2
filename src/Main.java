264bcb1c5822da515a1565c5dd69c20a78b9b2aa
Liniya Merin Mathew <Liniya@NTP-LAP-788.chn.nestgroup.net>
2022-11-21 12:46:30 +0530

    prime

--- src/Main.java
+++ src/Main.java
@@ -53,4 +53,26 @@ public class Main {
         smallest = c > temp ? temp : c;
         System.out.println("The smallest number is: " + smallest);
     }
+    public static void Prime() {
+        int i, n = 0, m = 0, flag = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the  number to check:");
+        n = sc.nextInt();
+        m = n / 2;
+        if (n == 0 || n == 1) {
+            System.out.println(n + " not a prime number");
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    System.out.println(n + " not a prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(n + " is a prime number");
+            }
+        }
+    }
+
 }
