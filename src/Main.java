f94a96019c9c802cadf13418219e7971ab80aa2a
Liniya Merin Mathew <Liniya@NTP-LAP-788.chn.nestgroup.net>
2022-11-21 12:54:58 +0530

    EvenOrOdd

--- src/Main.java
+++ src/Main.java
@@ -74,5 +74,18 @@ public class Main {
             }
         }
     }
+    public static void EvenOrOdd() {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if (num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+
 
 }
